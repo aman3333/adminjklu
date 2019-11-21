8bed4c11ec008b6aea5f49a1f2d6e41230225b2d
Aman Jain <amanjain.3331@gmail.com>
2019-11-21 15:21:17 +0530

    first commit

---
+++ precache-manifest.dda5b11a1d6ae0e57f6efc6af1ee205a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "d360b6ad08987d1f182c",
+    "url": "/static/js/main.d360b6ad.chunk.js"
+  },
+  {
+    "revision": "75b83db3e10e318e34c6",
+    "url": "/static/js/2.75b83db3.chunk.js"
+  },
+  {
+    "revision": "d360b6ad08987d1f182c",
+    "url": "/static/css/main.5f5f5ad2.chunk.css"
+  },
+  {
+    "revision": "409df5bd1791ddd621028d67f8d5211b",
+    "url": "/index.html"
+  }
+];
